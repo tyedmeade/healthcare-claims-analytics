b07a0237cd961bee94daed0d5112a250b95dce38
tyedmeade <tyedmeade@gmail.com>
2026-02-12 12:23:35 -0500

    Update revenue_leakage.sql

--- analysis_queries/revenue_leakage.sql
+++ analysis_queries/revenue_leakage.sql
@@ -1,1 +1,9 @@
-
+SELECT
+  payment_status_adjusted,
+  COUNT(*) AS claim_count,
+  SUM(claim_amount) AS total_claimed,
+  SUM(payment_amount) AS total_paid,
+  SUM(payment_gap) AS total_gap
+FROM vw_fact_claims
+GROUP BY payment_status_adjusted
+ORDER BY total_gap DESC;
