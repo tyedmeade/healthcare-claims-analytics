7dd9a252fe466ecbb8715f0012ebe7b1ad0dca3d
tyedmeade <tyedmeade@gmail.com>
2026-02-12 12:23:53 -0500

    Update payment_timeliness.sql

--- analysis_queries/payment_timeliness.sql
+++ analysis_queries/payment_timeliness.sql
@@ -1,1 +1,7 @@
-
+SELECT
+  DATE_TRUNC('month', claim_date) AS claim_month,
+  AVG(days_to_payment_clean) AS avg_days_to_payment
+FROM vw_fact_claims
+WHERE days_to_payment_clean IS NOT NULL
+GROUP BY claim_month
+ORDER BY claim_month;
